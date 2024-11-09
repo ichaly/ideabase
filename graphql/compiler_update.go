7cfc15d38b72abc5a7b0ff69e17c636fa6b4e68a
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-11-09 16:07:43 +0800

    feat: 更新数据使用正确的数据库类型

--- graphql/compiler_update.go
+++ graphql/compiler_update.go
@@ -1,17 +1,33 @@
 package graphql
 
 import (
+	"github.com/ichaly/ideabase/graphql/internal"
 	"github.com/samber/lo"
 	"github.com/vektah/gqlparser/v2/ast"
+	"strings"
 )
+
+type updateItem struct {
+	index int
+	field *internal.Field
+	value *ast.ChildValue
+}
 
 func (my *compilerContext) renderUpdate(id, pid int, f *ast.Field) {
 	update := f.Arguments.ForName(UPDATE)
-	table, _ := my.meta.TableName(f.Definition.Type.Name(), false)
+	class := strings.TrimSuffix(update.Value.Definition.Name, SUFFIX_UPDATE_INPUT)
+	table, _ := my.meta.TableName(class, false)
 
-	children := lo.Filter(update.Value.Children, func(item *ast.ChildValue, index int) bool {
+	children := lo.Map(lo.Filter(update.Value.Children, func(item *ast.ChildValue, index int) bool {
 		return item.Value.Definition.Kind == ast.Scalar
+	}), func(item *ast.ChildValue, index int) updateItem {
+		field, _ := my.meta.FindField(class, item.Name, false)
+		return updateItem{index: index, field: field, value: item}
 	})
+
+	//children := lo.Filter(update.Value.Children, func(item *ast.ChildValue, index int) bool {
+	//	return item.Value.Definition.Kind == ast.Scalar
+	//})
 
 	my.Quoted(table)
 	my.Space(`AS (UPDATE`)
@@ -21,17 +37,17 @@ func (my *compilerContext) renderUpdate(id, pid int, f *ast.Field) {
 		if i != 0 {
 			my.Write(`,`)
 		}
-		my.Quoted(v.Name)
+		my.Quoted(v.value.Name)
 	}
 	my.Space(`) = (SELECT`)
 	for i, v := range children {
 		if i != 0 {
 			my.Write(`,`)
 		}
-		raw, _ := v.Value.Value(my.variables)
+		raw, _ := v.value.Value.Value(my.variables)
 		my.Wrap(`'`, raw)
 		my.Write(`::`)
-		my.Write("text") //TODO:需要转化为数据库对应的具体类型
+		my.Write(v.field.DataType)
 	}
 	my.Space(`)`)
 
