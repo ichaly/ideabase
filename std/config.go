4817d37d7efddafa67d21854b584a65ebeb5b59d
shi.changliang <shi.changliang@zhaopin.com.cn>
2025-02-28 12:49:29 +0800

    feat: 为配置结构添加调试模式支持

--- std/config.go
+++ std/config.go
@@ -5,8 +5,10 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Config 表示标准配置
 type Config struct {
 	internal.AppConfig `mapstructure:"app"`
+	Debug              bool `mapstructure:"debug"`
 }
 
 func NewConfig(v *viper.Viper) (*Config, error) {
