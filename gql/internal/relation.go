24446d78a503ded7f240e25021e72b893d197a99
shi.changliang <shi.changliang@zhaopin.com.cn>
2025-06-23 14:09:11 +0800

    refactor: 调整文件存储位置

--- gql/internal/relation.go
+++ gql/internal/relation.go
@@ -16,3 +16,28 @@ type Through struct {
 	TargetKey string `json:"targetKey"` // 中间表中指向目标表的外键
 	SourceKey string `json:"sourceKey"` // 中间表中指向源表的外键
 }
+
+// RelationType 表示关系类型
+type RelationType string
+
+// 关系类型常量
+const (
+	RECURSIVE    RelationType = "recursive"    // 递归关系
+	MANY_TO_ONE  RelationType = "many_to_one"  // 多对一关系
+	ONE_TO_MANY  RelationType = "one_to_many"  // 一对多关系
+	MANY_TO_MANY RelationType = "many_to_many" // 多对多关系
+)
+
+// Parse 从字符串转换为关系类型
+func (my RelationType) Parse(kind string) RelationType {
+	switch kind {
+	case string(ONE_TO_MANY):
+		return ONE_TO_MANY
+	case string(MANY_TO_MANY):
+		return MANY_TO_MANY
+	case string(RECURSIVE):
+		return RECURSIVE
+	default:
+		return MANY_TO_ONE // 默认为多对一
+	}
+}
--- gql/internal/types.go
+++ gql/internal/types.go
@@ -1,51 +1,8 @@
 package internal
 
-// RelationType 表示关系类型
-type RelationType string
-
-// 关系类型常量
-const (
-	MANY_TO_ONE  RelationType = "many_to_one"  // 多对一关系
-	ONE_TO_MANY  RelationType = "one_to_many"  // 一对多关系
-	MANY_TO_MANY RelationType = "many_to_many" // 多对多关系
-	RECURSIVE    RelationType = "recursive"    // 递归关系
-)
-
 // Symbol 表示操作符号
 type Symbol struct {
 	Name        string
 	Value       string
 	Description string
-}
-
-// FromString 从字符串转换为关系类型
-func (my RelationType) FromString(kind string) RelationType {
-	switch kind {
-	case string(ONE_TO_MANY):
-		return ONE_TO_MANY
-	case string(MANY_TO_ONE):
-		return MANY_TO_ONE
-	case string(MANY_TO_MANY):
-		return MANY_TO_MANY
-	case string(RECURSIVE):
-		return RECURSIVE
-	default:
-		return MANY_TO_ONE // 默认为多对一
-	}
-}
-
-// Reverse 获取反向关系类型
-func (my RelationType) Reverse() RelationType {
-	switch my {
-	case ONE_TO_MANY:
-		return MANY_TO_ONE
-	case MANY_TO_ONE:
-		return ONE_TO_MANY
-	case MANY_TO_MANY:
-		return MANY_TO_MANY
-	case RECURSIVE:
-		return RECURSIVE
-	default:
-		return ONE_TO_MANY // 默认为一对多
-	}
 }
