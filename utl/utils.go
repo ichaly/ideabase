6fab424845a30763c3d4a72af15d5f9dcfa630f9
ichaly <i@eunut.com>
2025-07-08 21:57:44 +0800

    feat: 新增Must函数处理错误

--- utl/utils.go
+++ utl/utils.go
@@ -65,3 +65,11 @@ func RandomCode(width int) string {
 	}
 	return sb.String()
 }
+
+// Must 保证函数返回的错误不会为 nil，否则会 panic
+func Must[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
