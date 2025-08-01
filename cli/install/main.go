e2c9f3f6e9dd63b4842b8ed3b45b0d2adfe12862
shi.changliang <shi.changliang@zhaopin.com.cn>
2025-08-01 19:26:33 +0800

    build(cli): 重构 Makefile并添加 install 子命令

--- cli/install/main.go
+++ cli/install/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "install",
+		Short: "Install CLI 工具",
+		Long:  `Install CLI 工具用于安装 IdeaBase 项目的各种命令行工具`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Install CLI 工具")
+			fmt.Println("使用 'install --help' 查看更多选项")
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
